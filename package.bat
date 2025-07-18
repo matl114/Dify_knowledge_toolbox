6cfff75c0383399b224298ce7c6707a2b84fa5c4
m1919810 <matl040527@qq.com>
2025-07-18 11:23:52 +0800

    feat: add version number

--- package.bat
+++ package.bat
@@ -1,5 +1,9 @@
 @echo off
 for %%i in ("%CD%") do set "dir_name=%%~nxi"
-echo %dir_name%
 
-dify plugin package ./ -o ./target/%dir_name%.difypkg
+for /f "tokens=2 delims=: " %%a in ('findstr /r /c:"^version: *" "./manifest.yaml"') do (
+    set version=%%a
+)
+echo packing plugin version %version%
+dify plugin package ./ -o ./target/%dir_name%_%version%.difypkg
+echo packing finish
