b2c1482bbd8c856ff151c6bcf01e0dfefdf68236
m1919810 <matl040527@qq.com>
2025-07-18 12:44:56 +0800

    fix :cmd

--- package.bat
+++ package.bat
@@ -1,9 +1,11 @@
 @echo off
 for %%i in ("%CD%") do set "dir_name=%%~nxi"
 
-for /f "tokens=2 delims=: " %%a in ('findstr /r /c:"^version: *" "./manifest.yaml"') do (
+for /f "tokens=2 delims=: " %%a in ('findstr /r /c:"^version-tag: *" ".\target\package-config.yaml"') do (
     set version=%%a
 )
+
+
 echo packing plugin version %version%
 dify plugin package ./ -o ./target/%dir_name%_%version%.difypkg
 echo packing finish
